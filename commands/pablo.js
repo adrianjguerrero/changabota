385c1000c6c4bc501da1351e453ad0668d51bf6c
Adrian Guerrero <adrianjguerrero87@gmail.com>
2021-12-05 16:42:08 -0400

    comandos jaja

---
+++ commands/pablo.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'pablo',
+	description: 'el pepe!',
+	execute(message, args) {
+		message.channel.send('https://scontent-mia3-2.xx.fbcdn.net/v/t1.6435-9/152813756_719041058756796_7676434525851607501_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=uZMH-LT4LrsAX9aXAt0&tn=ausz61PBacCluKg9&_nc_ht=scontent-mia3-2.xx&oh=38b5036cac1e9cbaafa7b4d5ccca8776&oe=61CEDAE3');
+	}
+};
---
+++ commands/pepe.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'elpepe',
+	description: 'el pepe!',
+	execute(message, args) {
+		message.channel.send('ete sech ete sech');
+	},
+};
